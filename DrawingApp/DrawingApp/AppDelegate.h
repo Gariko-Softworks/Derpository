93392eceb0581fe8659bc81f7f6f3312c4fe8a39
Alexander Ikonomidis <xikonomidis@gmail.com>
2012-05-27 20:34:16 -0400

    Initial commit. Hopefully we ridin'

---
+++ DrawingApp/DrawingApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DrawingApp
+//
+//  Created by Alexander Ikonomidis on 5/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DrawingApp/DrawingApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DrawingApp
+//
+//  Created by Alexander Ikonomidis on 5/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
